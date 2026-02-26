27edf6152eb01c13705f34ded50b5f9f8926aff8
pjg09 <pedroj1229@gmail.com>
2026-02-26 17:14:43 -0500

    feat: Seteo de colores y temas

--- app/src/main/java/com/upb/obsia/ui/theme/Color.kt
+++ app/src/main/java/com/upb/obsia/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.upb.obsia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val FondoPrincipal = Color(0xFF36B8B8)
+val FondoBlanco = Color(0xFFFFFFFF)
+val FondoChat = Color(0xFFE9FEFE)
+val BlancoAuxiliar = Color(0xFFF9FFFF)
+val BlancoAuxiliar2 = Color(0xFFFCFFFF)
+val LetrasNegras = Color(0xFF191919)
+val IconosMenu = Color(0xFFD9D9D9)
+val FondoAzulClaro = Color(0xFFB5E2E2)
+val MensajesUsuario = Color(0xFF267D7D)
+val AzulAuxiliarChat = Color(0xFFB6DEDE)
+val Negro = Color(0xFF000000)
