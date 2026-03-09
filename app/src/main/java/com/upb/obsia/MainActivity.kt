b580865f4d45c01731c46f70b612c2c2c241ed21
pjg09 <pedroj1229@gmail.com>
2026-03-09 14:19:16 -0500

    fix: eliminacion de codigo de prueba de android studio

--- app/src/main/java/com/upb/obsia/MainActivity.kt
+++ app/src/main/java/com/upb/obsia/MainActivity.kt
@@ -1,16 +1,12 @@
+// app/src/main/java/com/upb/obsia/MainActivity.kt
+
 package com.upb.obsia
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.upb.obsia.ui.theme.ObsIATheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ObsIATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ObsIATheme {
-        Greeting("Android")
     }
 }
