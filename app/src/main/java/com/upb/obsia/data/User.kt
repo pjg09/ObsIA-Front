403fc06d32795d90324f465180a7a1ffe7a2982c
pjg09 <pedroj1229@gmail.com>
2026-03-14 18:34:26 -0500

    fix: Room lista para el register, register funcional con validaciones y control de usuarios duplicados

--- app/src/main/java/com/upb/obsia/data/User.kt
+++ app/src/main/java/com/upb/obsia/data/User.kt
@@ -1,12 +1,17 @@
 package com.upb.obsia.data
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "users")
+@Entity(
+        tableName = "users",
+        indices =
+                [Index(value = ["celular"], unique = true), Index(value = ["email"], unique = true)]
+)
 data class User(
         @PrimaryKey(autoGenerate = true) val id: Int = 0,
         val nombre: String,
         val email: String,
-        val celular: String,
+        val celular: String
 )
